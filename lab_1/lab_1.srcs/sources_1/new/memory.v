f8385110ebf031d7b275f45e581adf94faafc147
Curse-Ka <wkrska@bu.edu>
2022-10-09 18:25:32 -0400

    Basic ALU function

--- lab_1/lab_1.srcs/sources_1/new/memory.v
+++ lab_1/lab_1.srcs/sources_1/new/memory.v
@@ -1,35 +1,18 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/06/2022 04:36:30 PM
-// Design Name: 
-// Module Name: memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module memory(
 	input wen,
     input [31:0] din,
-    input [11:0] raddr,
+    input [11:0] raddr_i, // read addr instruction
+    input [11:0] raddr_d, // read addr data
     input [11:0] waddr,
+    output [31:0] iout,
     output [31:0] dout
     );
 
     reg [31:0] mem [11:0];
     
+    // init to 0;
     integer i;
     initial begin
         for (i = 0; i < 12'hFFF; i=i+1) begin
@@ -37,7 +20,11 @@ module memory(
         end
     end
 
+    // Assign mem on valid input
     assign mem[waddr] = (wen) ? din : mem[waddr];
-    assign dout = mem[raddr];
-    
+
+    // Assign output
+    assign iout = mem[raddr_i];
+    assign dout = mem[raddr_d];
+
 endmodule
