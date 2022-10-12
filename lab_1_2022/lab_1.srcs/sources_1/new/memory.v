3f8435eef01159da8e4566000576a3cba1854cd2
William Krska <wkrska@bu.edu>
2022-10-12 15:03:01 -0400

    file reorg

---
+++ lab_1_2022/lab_1.srcs/sources_1/new/memory.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2022 02:36:50 PM
+// Design Name: 
+// Module Name: memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module memory(
+
+    );
+endmodule
