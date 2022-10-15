f104d0521d1f3b7310a596d2025a3fcf1497b230
William Krska <wkrska@bu.edu>
2022-10-14 21:24:26 -0400

    rework datapath.v

--- lab_1/lab_1.srcs/sources_1/imports/datapath.v
+++ lab_1/lab_1.srcs/sources_1/imports/datapath.v
@@ -5,60 +5,95 @@ module datapath (
     input rst
 );
     //----------- IF stage -----------//
-    wire [15:0] INSTR_IFID, INSTR_IDEX;
-    wire [11:0] PC_IFID, PC_IDEX;
-    wire PCEN_IFID, PCEN_IDEX;
+    wire [15:0] INST_next, INST_curr, INST_read;
+    wire [11:0] PC_next, PC_curr;
+    wire PC_en;
+
+    wire [3:0]  RN_ID, RM_ID, OP_ID;
+    wire [3:0]  RN_EX, RM_EX, OP_EX;
+    wire [3:0]  RN_WB, RM_WB, OP_WB;
+    wire [15:0] RF_D1_ID, RF_D2_ID, MEM_D_ID;
+    wire [15:0] RF_D1_EX, RF_D2_EX, MEM_D_EX;
+    wire [15:0]           RF_D2_WB // Would have target address for Move [Rn], [Rm]
+    wire [11:0] imm_ID;
+
+    wire [15:0] RES_EX, RES_WB;
+    wire FLAG_EX, FLAG_WB;
 
     Fetch_unit fetch_0(
-        .clk(.clk),
-        .Address(),
-        .Instruction(INSTR_IDEX),
+        .PC_in(PC_curr),
+        .inst_in(INST_read),
+        .imm_in(),
+        .ctrl_jump(),
+        .ctrl_nop(),
+        .inst_out(INST_next),
+        .PC_out(PC_next),
+        .PC_en(PC_en),
     );
 
-    reg_param reg0 #(.SIZE(16+12+1)) (
+    reg_param reg0 #(.SIZE(16)) (
         .clk(clk),
-        .din({INSTR_IFID, PC_IFID, PCEN_IFID}),
-        .dout({INSTR_IDEX, PC_IDEX, PCEN_IDEX})
+        .rst(rst),
+        .din(INST_next),
+        .dout(INST_curr)
     );
 
     //----------- ID stage -----------//
-    wire [3:0] RM_IDEX, RN_IDEX;
-    wire [15:0] RD1_IDEX, RD2_IDEX;
-    wire [11:0] PC_IDEX;
-    wire [3:0] OP_IDEX;
-    wire [11:0] imm_IDEX;
-    // wire [31:0] RM_EXMEM, RN_EXMEM;
-    // wire [31:0] RM_MEMWB, RN_MEMRB;
+    
+
     Decoder decoder_0(
-        .Instr_in(INSTR_IDEX),
-        .RM(RM_IDEX),
-        .RN(RN_IDEX),
-        .imm(imm_IDEX)
+        .Instr_in(INST_curr),
+        .Rm(RM_ID),
+        .Rn(RN_ID),
+        .op(OP_ID)
+        .imm(imm_ID)
     );
 
     Register_file rf_0(
         .clk(clk),
         .rst(rst),
-        .RS1(RM_IDEX),
-        .RS2(RN_IDEX),
+        .RS1(RM_ID),
+        .RS2(RN_ID),
         .WS(),
         .WE(),
-        .PC_IN(PC_IDEX),
-        .PC_EN(PCEN_IDEX)
-        .RD1(RD1_IDEX),
-        .RD2(RD2_IDEX),
-        .PC_OUT(PC_IDEX),
+        .PC_IN(PC_next),
+        .PC_EN(PC_en)
+        .RD1(RF_D1_ID),
+        .RD2(RF_D2_ID), // corresponds to RM
+        .PC_OUT(PC_curr),
     );
 
-    // EX stage
-    ALU alu_0(
-
-    );
-
-    // Mem stage
     memory mem_0(
-
+        .rst(rst),
+        .raddr_i(PC_curr),
+        .raddr_d(RF_D2_ID),
+        .waddr(),
+        .din(),
+        .wen(),
+        .iout(INST_read),
+        .dout(MEM_D_ID)
     );
-    
+
+    reg_param reg1 #(.SIZE(4+4+4+16+16+16)) (
+        .clk(clk),
+        .rst(rst),
+        .din({RN_ID, RM_ID, OP_ID, RF_D1_ID, RF_D2_ID, MEM_D_ID}),
+        .dout({RN_EX, RM_EX, OP_EX, RF_D1_EX, RF_D2_EX, MEM_D_EX})
+    );
+
+    //----------- EX stage -----------//
+    ALU alu_0(
+        .op(OP_EX),
+        .din0(RF_D1_EX),
+        .din1((OP_EXWB == 4'b0000) ? RF_D2_EX : MEM_D_EX),
+        .dout(RES_EX)
+    );
+
+    reg_param reg2 #(.SIZE(16+1)) (
+        .clk(clk),
+        .rst(rst),
+        .din({RES_EX, J_FLAG}),
+        .dout({RES_WB, J_FLAG_WB})
+    );
 
 endmodule
--- lab_1/lab_1.srcs/sources_1/imports/reg_param.v
+++ lab_1/lab_1.srcs/sources_1/imports/reg_param.v
@@ -2,12 +2,16 @@
 
 module reg_param #(parameter SIZE = 4) (
     input wire clk,
+    input wire rst,
     input wire [SIZE-1:0] din,
     output reg [SIZE-1:0] dout
 );
 
 always @(posedge clk) begin
-    dout <= din;
+    if (rst)
+        dout <= 'b0;
+    else
+        dout <= din;
 end
 
 endmodule
