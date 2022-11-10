f83ac5012f9568a4a644ca5bd4c92054c60b0154
Curse-Ka <wkrska@bu.edu>
2022-11-10 18:30:43 -0500

    ascii lookup

---
+++ Lab2/Unified Terminal Interface/Sources/Design/ps2_to_ascii.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+
+`include "my_header.vh"
+
+module ps2_to_ascii(
+    input wire [7:0] ps2,
+    output reg [7:0] ascii
+);
+
+always @(*) begin
+    case(ps2) // 0X is number, 1X is char
+        8'h45: ascii = 8'h30; // 0
+        8'h70: ascii = 8'h30; // 0
+        8'h16: ascii = 8'h31; // 1
+        8'h69: ascii = 8'h31; // 1
+        8'h1E: ascii = 8'h32; // 2
+        8'h72: ascii = 8'h32; // 2
+        8'h26: ascii = 8'h33; // 3
+        8'h7A: ascii = 8'h33; // 3
+        8'h25: ascii = 8'h34; // 4
+        8'h6B: ascii = 8'h34; // 4
+        8'h2E: ascii = 8'h35; // 5
+        8'h73: ascii = 8'h35; // 5
+        8'h36: ascii = 8'h36; // 6
+        8'h74: ascii = 8'h36; // 6
+        8'h3D: ascii = 8'h37; // 7
+        8'h6C: ascii = 8'h37; // 7
+        8'h3E: ascii = 8'h38; // 8
+        8'h75: ascii = 8'h38; // 8
+        8'h46: ascii = 8'h39; // 9
+        8'h7D: ascii = 8'h39; // 9
+        8'h4E: ascii = 8'h2D; // -
+        8'h7B: ascii = 8'h2D; // -
+        8'h7C: ascii = 8'h2A; // *
+        8'hE0: ascii = 8'h2F; // /
+        8'h79: ascii = 8'h2B; // +
+        8'h55: ascii = 8'h3D; // =
+        8'h5A: ascii = 8'hA; // Enter
+        8'hE0: ascii = 8'hA; // Enter
+        8'h12: ascii = 8'h0; // Shift (Left)
+        8'h59: ascii = 8'h0; // Shift (Right)
+        8'h29: ascii = 8'h20; // Spacebar
+        8'h1C: ascii = 8'h41; // A
+        8'h32: ascii = 8'h42; // B
+        8'h21: ascii = 8'h43; // C
+        8'h23: ascii = 8'h44; // D
+        8'h24: ascii = 8'h45; // E
+        8'h2B: ascii = 8'h46; // F
+        8'h34: ascii = 8'h47; // G
+        8'h33: ascii = 8'h48; // H
+        8'h43: ascii = 8'h49; // I
+        8'h3B: ascii = 8'h4A; // J
+        8'h42: ascii = 8'h4B; // K
+        8'h4B: ascii = 8'h4C; // L
+        8'h3A: ascii = 8'h4D; // M
+        8'h31: ascii = 8'h4E; // N
+        8'h44: ascii = 8'h4F; // O
+        8'h4D: ascii = 8'h50; // P
+        8'h15: ascii = 8'h51; // Q
+        8'h2D: ascii = 8'h52; // R
+        8'h1B: ascii = 8'h53; // S
+        8'h2C: ascii = 8'h54; // T
+        8'h3C: ascii = 8'h55; // U
+        8'h2A: ascii = 8'h56; // V
+        8'h1D: ascii = 8'h57; // W
+        8'h22: ascii = 8'h58; // X
+        8'h35: ascii = 8'h59; // Y
+        8'h1A: ascii = 8'h5A; // Z
+        default: trans_key_ps2 = 8'h0; // D/C
+    endcase
+end
+
+endmodule
--- Lab2/Unified Terminal Interface/Sources/Design/uart_arbiter.v
+++ Lab2/Unified Terminal Interface/Sources/Design/uart_arbiter.v
@@ -5,11 +5,63 @@
 
 // takes the input from UART and generates a sustained key value and pulsed wen on the PS/2 clk
 module uart_arbiter(
-    input clk_100,
-    input clk_ps2,
-    input rst,
-    input [7:0] uart_dat,
-    input uart_dv,
-    output uart_dat_sus,
-    output uart_wen
+    input wire clk_100,
+    input wire clk_ps2,
+    input wire rst,
+    input wire [7:0] uart_dat,
+    input wire uart_dv,
+    output reg key_uart,
+    output reg wen_uart
 );
+
+// Two interconnected FSMs, on system clock and ps2 clock respectively
+//  >0 IDLE, waits for UART signal in, uart signal sustained as previous value
+//  >1 set off ps2 fsm, wait for it to enter state 1
+//  >2 wait for valid to go low
+
+// Latch UART inputs
+reg [1:0] uart_cs, uart_ns;
+reg wen_uart_n;
+reg [7:0] key_uart_n;
+
+localparam [1:0] IDLE = 2'b00,
+                 FLAG = 2'b01,
+                 WAIT = 2'b10;
+
+always @(posedge clk_100) begin
+    if (rst) begin
+        uart_cs <= 'b0;
+        wen_uart <= 'b0;
+        key_uart <= 'b0;
+    end else begin
+        uart_cs <= uart_ns;
+        wen_uart <= wen_uart_n;
+        key_uart <= key_uart_n;
+    end
+end
+
+always @(posedge clk_ps2) begin
+    if (rst) begin
+        uart_wen <= 1'b0;
+    end else begin
+        uart_wen <= (uart_cs == FLAG) ? 1'b1 : 1'b0; // will go high for the duration of a ps2 clock only once per input key
+    end
+end
+
+always @(*) begin
+    case (uart_cs)
+        IDLE: begin
+            uart_ns = (uart_dv) ? FLAG : IDLE;
+            wen_uart_n = 1'b0;
+            key_uart_n = key_uart;
+        end
+        FLAG: begin
+            uart_ns = (uart_dv) ? FLAG : IDLE;
+        end
+        WAIT: begin
+            
+        end
+    endcase
+end
+
+endmodule
